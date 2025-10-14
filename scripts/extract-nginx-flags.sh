7b6f1061ef5988b7c0681ba1f32e0f9bfa774db6
Benjamin Hermit <72666597+Benjamin1919@users.noreply.github.com>
2025-10-14 22:30:43 +0800

    Update extract-nginx-flags.sh

--- scripts/extract-nginx-flags.sh
+++ scripts/extract-nginx-flags.sh
@@ -4,11 +4,12 @@ set -e
 # 从 nginx-full 包中提取 configure 参数
 #apt-get update -qq
 apt-get source -qq nginx
-cd nginx-* || exit 1
+cd nginx-*/debian || exit 1
 FLAGS=$(grep -oP '(?<=--configure-args=).*' rules | tail -1 | sed 's/"//g')
 
 if [[ -z "$FLAGS" ]]; then
   echo "❌ 未找到 nginx-full 的 configure 参数"
+  echo "NGINX_CONFIGURE_FLAGS=" >> $GITHUB_ENV
   exit 1
 fi
 
