6bd7f37e93b17920abed6a736efcfeee3e4e3610
Zephyrine Thompson <72666597+Benjamin1919@users.noreply.github.com>
2025-10-15 23:30:31 +0800

    Update configure-flags.sh

--- scripts/configure-flags.sh
+++ scripts/configure-flags.sh
@@ -46,6 +46,6 @@ CONFIGURE_FLAGS="--prefix=/usr/local/nginx \
 --with-stream_ssl_preread_module \
 --with-compat"
 
-echo "CONFIGURE_FLAGS<<EOF" >> $GITHUB_ENV
-echo "$CONFIGURE_FLAGS" >> $GITHUB_ENV
-echo "EOF" >> $GITHUB_ENV
+CONFIGURE_FLAGS_FORMAT=$(echo "$CONFIGURE_FLAGS" | tr -d '\\' | tr '\n' ' ' | sed 's/  */ /g' | xargs)
+
+echo "CONFIGURE_FLAGS=$CONFIGURE_FLAGS_FORMAT" >> $GITHUB_ENV
