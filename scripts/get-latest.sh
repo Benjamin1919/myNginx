e72ddf1fa49ece114265498779f5aa82351f6deb
Zephyrine Thompson <72666597+Benjamin1919@users.noreply.github.com>
2025-10-17 00:38:34 +0800

    Update get-latest.sh

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -8,14 +8,14 @@ NGINX_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://
 OPENSSL_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/openssl/openssl/releases" | grep -Po '"tag_name":\s*"openssl-\K[0-9A-Za-z.\-]+' | grep -Evi '(alpha|beta)' | sort -V | tail -1)
 
 if [ -z "$NGINX_VERSION" ]; then
-  echo "❌ Failed to fetch Nginx latest versions, fallback to 1.29.2"
+  echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.2"
   NGINX_VERSION=1.29.2
 else
   echo "✅ NGINX_VERSION=$NGINX_VERSION"
 fi
 
 if [ -z "$OPENSSL_VERSION" ]; then
-  echo "❌ Failed to fetch OpenSSL latest versions, fallback to 3.6.0"
+  echo "❌ Failed to fetch OpenSSL latest version, fallback to 3.6.0"
   OPENSSL_VERSION=3.6.0
 else
   echo "✅ OPENSSL_VERSION=$OPENSSL_VERSION"
