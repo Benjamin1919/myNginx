2b4587f89c94a684b8ceb45b84675ff078b2c7d3
Zephyrine Thompson <72666597+Benjamin1919@users.noreply.github.com>
2025-10-17 20:00:25 +0800

    update

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Fetching latest Nginx and OpenSSL versions..."
+echo "Fetching latest versions..."
 
-NGINX_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/nginx/nginx/releases" | grep -Po '"tag_name":\s*"release-\K[0-9.]+' | sort -V | tail -1)
+NGINX_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/nginx/nginx/releases/latest" | jq -r '.tag_name | sub("^release-"; "")')
 
-OPENSSL_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/openssl/openssl/releases" | grep -Po '"tag_name":\s*"openssl-\K[0-9A-Za-z.\-]+' | grep -Evi '(alpha|beta)' | sort -V | tail -1)
+OPENSSL_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/openssl/openssl/releases/latest" | jq -r '.tag_name | sub("^openssl-"; "")')
+
+ZLIB_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/madler/zlib/releases/latest" | jq -r '.tag_name | sub("^zlib-"; "")')
+
+PCRE2_VERSION=$(curl -s --connect-timeout 10 --retry 2 --retry-delay 5 "https://api.github.com/repos/PCRE2Project/pcre2/releases/latest" | jq -r '.tag_name | sub("^pcre2-"; "")')
 
 if [ -z "$NGINX_VERSION" ]; then
   echo "❌ Failed to fetch Nginx latest version, fallback to 1.29.2"
@@ -21,7 +25,23 @@ else
   echo "✅ OPENSSL_VERSION=$OPENSSL_VERSION"
 fi
 
+if [ -z "$ZLIB_VERSION" ]; then
+  echo "❌ Failed to fetch zlib latest version, fallback to 1.3.1"
+  PCRE2_VERSION=1.3.1
+else
+  echo "✅ ZLIB_VERSION=$ZLIB_VERSION"
+fi
+
+if [ -z "$PCRE2_VERSION" ]; then
+  echo "❌ Failed to fetch PCRE2 latest version, fallback to 10.46"
+  PCRE2_VERSION=10.46
+else
+  echo "✅ PCRE2_VERSION=$PCRE2_VERSION"
+fi
+
 {
   echo "NGINX_VERSION=${NGINX_VERSION}"
   echo "OPENSSL_VERSION=${OPENSSL_VERSION}"
+  echo "ZLIB_VERSION=${ZLIB_VERSION}"
+  echo "PCRE2_VERSION=${PCRE2_VERSION}"
 } >> "$GITHUB_ENV"
--- scripts/sync-cfg-flags.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-CONFIGURE_FLAGS_RAW=$(tr '\n' ' ' < configure/parameters.txt | xargs)
-
-echo "CONFIGURE_FLAGS_RAW=$CONFIGURE_FLAGS_RAW" >> $GITHUB_ENV
